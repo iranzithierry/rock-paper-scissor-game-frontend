48236d178688141478524caa85f19ace1ad9d9a2
Iranzi Dev   <iranzithierry12@gmail.com>
2024-05-25 17:49:32 +0200

    export const runtime = 'edge'

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -4,6 +4,7 @@ import { getSession, updateAccessToken } from './lib/sessions';
 import COOKIE_NAMES from './constants/cookies-names';
 import { createGame } from './lib/game';
 
+export const runtime = 'edge'
 export async function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname
     if(process.env.MAINTENANCE != "true"){
