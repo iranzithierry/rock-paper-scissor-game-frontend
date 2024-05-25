02566997d97065100c8d859d0779f7b2a06b6445
Iranzi Dev   <iranzithierry12@gmail.com>
2024-05-25 16:20:54 +0200

    idk

--- backend/src/asgi.py
+++ backend/src/asgi.py
@@ -1,21 +1,24 @@
 import os
-from src import routing
-from pathlib import Path
 import sys
+from pathlib import Path
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-
-from src.games.asgi.middleware import JwtAuthMiddlewareStack
 
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
 sys.path.append(str(ROOT_DIR / "src"))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings.local")
 
+# Ensure the settings are configured and the application is ready before importing other modules
+django_asgi_app = get_asgi_application()
+
+from src import routing  # Import routing after initializing Django ASGI app
+from src.games.asgi.middleware import JwtAuthMiddlewareStack  # Import middleware after initializing Django ASGI app
+
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             JwtAuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
         ),
