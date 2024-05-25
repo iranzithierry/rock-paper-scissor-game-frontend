102fa3c650494308471659e01e74e99b0a488545
Iranzi Dev   <iranzithierry12@gmail.com>
2024-05-25 12:20:01 +0200

    for the backend

--- backend/src/games/admin.py
+++ backend/src/games/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from src.games.models import Game, Interaction, Choice
 
 # Register your models here.
+admin.site.register(Game)
+admin.site.register(Choice)
+admin.site.register(Interaction)
