be231680783a105209310555a9ed5caea8f18eb3
Iranzi Dev   <iranzithierry12@gmail.com>
2024-05-25 15:15:20 +0200

    Update game-context.tsx

--- frontend/contexts/game-context.tsx
+++ frontend/contexts/game-context.tsx
@@ -65,7 +65,7 @@ export const GameContextProvider: React.FC<{ children: ReactNode; gameId: string
 
   const { sendJsonMessage } = useWebSocket(
     // `ws://${BACKEND_HOST.split("//")[1]}/ws/game/${gameId}/`,
-    `https://skizzy.ebuzzie.com/ws/game/${gameId}/`,
+    `https://skizzy.ebuzzie.com:8000/ws/game/${gameId}/`,
     {
       queryParams: { token: accessToken ?? "" },
       onMessage: handleWebSocketMessage,
